f70120cab1dba519e0b2777adb098bd50d826dec
nicholascm90 <nicholascm90@gmail.com>
2017-03-26 23:46:48 +0000

    files moved

--- client/app.js
+++ client/app.js
@@ -4,5 +4,15 @@ let angular = angular;
 
 let bestByMeApp = angular.module('bestByMeApp', ['ngRoute']); 
 
-
+bestByMeApp.controller('HomeCtrl', () => {
+    this.sample = 'sample'; 
+}); 
+bestByMeApp.config(['$routeProvider'], ($routeProvider) => {
+    $routeProvider.when('/home', {
+        templateUrl: '../views/home.html',
+        controller: 'HomeCtrl', 
+        controllerAs: 'home', 
+        access: {restricted: false}
+    }); 
+}); 
 
